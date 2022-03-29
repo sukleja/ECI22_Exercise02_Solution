8230f1889165e108a44c025d850554d0f587b27a
sebastian.ukleja <sebastian.ukleja@fh-campuswien.ac.at>
2022-03-29 10:31:54 +0200

    Added comments

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 def count_integer(numbers, integer):
-    '''
-    Counts the occurences of the given integer in a given array
-    :param numbers: 2 integer array, integer
-    :return: count of the integer in the array as integer
-    '''
     count = 0
     for index, value in enumerate(numbers):
         if value == integer:
@@ -15,11 +10,9 @@ def count_integer(numbers, integer):
 def list_func(numbers, integer):
     if integer not in numbers:
         return []
-    # replace integer with 6
     for index, value in enumerate(numbers):
         if value == integer:
-            numbers.pop(index)
-            numbers.insert(index, 6)
+            numbers[index] = 6
     tmp_list = numbers.copy()
     tmp_list.reverse()
     print(tmp_list)
@@ -28,22 +21,24 @@ def list_func(numbers, integer):
 def compare_lists(list1, list2):
     result=[]
     for x in list1:
+        #this verion only adds one of the common elements to the new list
         if x in list2 and x not in result:
             result.append(x)
     return result
 
-def remove_duplicates(text):
-    result = []
-    tmp_set = set(text)
-    if len(tmp_set) != len(text):
+def remove_duplicates(lst):
+    result = list(lst) # making sure that the given argument will become a list
+    tmp_set = set(lst)
+    if len(tmp_set) != len(lst):
         result = list(tmp_set)
     return result
 
 def dict_func(dictionary):
+    '''The .setdefault() function works like the .get function, the get function
+    fetches the value from the key and returns a given default if no key was found, the .setdefault function does the
+    same but also adds the default key and value to the dictioniary if no key was found'''
     print(
-        f'You have a {dictionary.get("Type", "unknown type")} from {dictionary.get("Brand", "unknown brand")} that costs: {dictionary.get("Price", "an unknown amount")}')
+        f'You have a {dictionary.setdefault("Type", "unknown type")} from {dictionary.setdefault("Brand", "unknown brand")} that costs: {dictionary.setdefault("Price", "an unknown amount")}')
     dictionary.setdefault("OS", "Linux")
     return dictionary
-
-
 
